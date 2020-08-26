e6da8d9a12b99660503563505d35728e03b816f1
Marko Arsovic <marko.arsovic@edu.ece.fr>
2020-08-26 21:17:37 +0200

    Deleting generated files

--- clear.sh
+++ clear.sh
@@ -5,6 +5,8 @@ cd $(dirname $([ -L $0 ] && readlink -f $0 || echo $0))
 # Deleting all generated files
 cd gen
 find . -name "*.py" -type f -delete
+find . -name "generated.sh" -type f -delete
+
 cd trash
 find . -name "*.py" -type f -delete
 
--- gen/generated.sh
+++
@@ -1,8 +0,0 @@
-osascript -e 'tell app "Terminal" 
- do script " /usr/bin/python3 /Users/arsovicmarko/Desktop/CodeGenerator/gen/trash/generated1.py 
- " 
-end tell' 
-osascript -e 'tell app "Terminal" 
- do script " /usr/bin/python3 /Users/arsovicmarko/Desktop/CodeGenerator/gen/trash/generated2.py 
- " 
-end tell' 
